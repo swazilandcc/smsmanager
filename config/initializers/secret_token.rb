174fa51c6d0cd2ee03a89ac5c141850cef52ac8b
Teddy A <teddya2007@gmail.com>
2013-05-16 16:26:07 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsmanager::Application.config.secret_token = 'b095563462d71a0e89a45b37de7b5f86424e4c82859eff32d0ef887767a71f8e8fda05caaf03e9cd232544430bd2ee9e4eb4e8e54c7af1237e20b9abae87ca02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Smsmanager::Application.config.session_store :cookie_store, key: '_smsmanager_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Smsmanager::Application.config.session_store :active_record_store
