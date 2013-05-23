628c00884c265bdb3f095cd473c6a7adc1a2d5a1
Teddy A <teddya2007@gmail.com>
2013-05-23 12:26:38 +0200

    Submit Systems

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,10 @@ class DashboardController < ApplicationController
     @bulk_message_template_count = BulkMessageTemplate.all.count()
     @incoming_messages_today = IncomingMessage.all(:conditions => "created_at BETWEEN '#{Time.now.strftime("%Y-%m-%d")} 00:00:00' AND '#{Time.now.strftime("%Y-%m-%d")} 23:59:59'").count()
     @incoming_messages_this_month = IncomingMessage.all(:conditions => "created_at BETWEEN '#{Time.now.strftime("%Y-%m")}-01 00:00:00' AND '#{Time.now.strftime("%Y-%m")}-31 23:59:59'").count()
-    @active_competitions = Competition.find_all_by_active(true).count()
+    @active_competitions = Competition.find_all_by_active(true)
+
+    @outgoing_messages_today = SmsLog.all(:conditions => "created_at BETWEEN '#{Time.now.strftime("%Y-%m-%d")} 00:00:00' AND '#{Time.now.strftime("%Y-%m-%d")} 23:59:59'").count()
+    @outgoing_messages_this_month = SmsLog.all(:conditions => "created_at BETWEEN '#{Time.now.strftime("%Y-%m")}-01 00:00:00' AND '#{Time.now.strftime("%Y-%m")}-31 23:59:59'").count()
   end
 
 end
--- app/workers/incoming_sms_worker.rb
+++ app/workers/incoming_sms_worker.rb
@@ -4,36 +4,104 @@ class IncomingSmsWorker
 
   def perform(sender, keyword, option, extra_text, message_to_send)
 
-    @incoming_message = IncomingMessage.new
-
     if (sender.nil? == false)
 
-      @incoming_message.sender = sender
-      @incoming_message.keyword = keyword
-      @incoming_message.option = option
-      @incoming_message.extra_text = extra_text
-      @incoming_message.matched_to_competition = false
-      @incoming_message.matched_to_devotional = false
+      @cleaned_up_keyword = keyword.to_s.upcase.strip! rescue nil
 
-      if @incoming_message.save!
+      @competition = Competition.where(:conditions => "active = 1 AND keyword = #{@cleaned_up_keyword} AND end_date >= #{Time.now.strftime("%Y-%m-%d")}").first
 
-        @send_response = SendSMS.new
-        @send_response.momt = "MT"
-        @send_response.sender = "7070"
-        @send_response.receiver = sender
-        @send_response.msgdata = mess
-        @send_response.sms_type = 2
+      if @competition.nil? == false
 
-        if @send_response.save!
+        @incoming_message = IncomingMessage.new
 
-          @incoming_message.reply_message = @send_response.msgdata
-          @incoming_message.reply_sent = true
-          @incoming_message.reply_sent_date_time = Time.now
-          @incoming_message.save!
+        if @competition.competition_options.exists? == true && option.nil? == false
+
+           @competition.competition_options.each do |x|
+
+              if option == x.option_number
+
+                @matches_option = true
+
+              else
+
+                @matches_option = false
+
+              end
+
+           end
+
+        end
+
+        @incoming_message.sender = sender
+        @incoming_message.keyword = keyword
+        @incoming_message.option = option
+        @incoming_message.extra_text = extra_text
+        @incoming_message.matched_to_competition = true
+        @incoming_message.competition_id  = @competition.id
+        @incoming_message.matched_to_devotional = false
+
+        if @incoming_message.save!
+
+          @send_response = SendSMS.new
+          @send_response.momt = "MT"
+          @send_response.sender = "7070"
+          @send_response.receiver = sender
+
+          if @matches_option = true
+
+            @send_response.msgdata = @competition.success_message
+
+          else
+
+            @send_response.msgdata = @competition.incorrect_option_message
+
+          end
+
+          @send_response.sms_type = 2
+
+          if @send_response.save!
+
+            @incoming_message.reply_message = @send_response.msgdata
+            @incoming_message.reply_sent = true
+            @incoming_message.reply_sent_date_time = Time.now
+            @incoming_message.save!
+
+          end
 
         end
 
 
+      else
+
+        @incoming_message = IncomingMessage.new
+
+        @incoming_message.sender = sender
+        @incoming_message.keyword = keyword
+        @incoming_message.option = option
+        @incoming_message.extra_text = extra_text
+        @incoming_message.matched_to_competition = false
+        @incoming_message.matched_to_devotional = false
+
+
+        if @incoming_message.save!
+
+          @send_response = SendSMS.new
+          @send_response.momt = "MT"
+          @send_response.sender = "7070"
+          @send_response.receiver = sender
+          @send_response.msgdata = message_to_send
+          @send_response.sms_type = 2
+
+          if @send_response.save!
+
+            @incoming_message.reply_message = @send_response.msgdata
+            @incoming_message.reply_sent = true
+            @incoming_message.reply_sent_date_time = Time.now
+            @incoming_message.save!
+
+          end
+
+        end
 
       end
 
